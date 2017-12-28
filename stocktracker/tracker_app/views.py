b676f9209f3fdb1cdccb214f500ac3f9f8d6dbf7
chrisbrickey <cbrickey1@gmail.com>
2017-12-27 19:54:29 -0800

    add custom view that takes query string and update top level view to show list of stocks available

--- stocktracker/tracker_app/views.py
+++ stocktracker/tracker_app/views.py
@@ -17,9 +17,11 @@ class StockList(generics.ListCreateAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
 
+
 class StockDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -27,5 +29,16 @@ def api_root(request, format=None):
         'stocks': reverse('stock-list', request=request, format=format)
     })
 
+
+
+
+#playing around with frontend
 def index(request):
-    return HttpResponse("i'm in control")
+    stock_list = Stock.objects.order_by('symbol')[:100] #pulls first 100 stocks based on symbol ABC order
+    stock_string = ', '.join([stock.symbol for stock in stock_list])
+    response = "Here is a list of all stocks in our database:\n" + stock_string
+    return HttpResponse(response)
+
+def custom_method_test(request, query_string):
+    response = "custom method received: %s"
+    return HttpResponse(response % query_string)
