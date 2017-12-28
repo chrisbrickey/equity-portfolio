68e6a82a8a9cbc407bfae38c6083e9f249c027b8
chrisbrickey <cbrickey1@gmail.com>
2017-12-27 17:37:31 -0800

    draft List and Detail views for Stock

--- stocktracker/tracker_app/views.py
+++ stocktracker/tracker_app/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Stock
+from .serializers import StockSerializer
+
+from rest_framework import generics
+
+class StockList(generics.ListCreateAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
