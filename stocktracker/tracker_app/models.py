5fb28fb2a7b5b041b3c7af46199ed69a7454f8c5
chrisbrickey <cbrickey1@gmail.com>
2017-12-28 12:29:49 -0800

    add Portfolio model and association within Stock

--- stocktracker/tracker_app/models.py
+++ stocktracker/tracker_app/models.py
@@ -10,6 +10,17 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
+class Portfolio(models.Model):
+
+    #unique portfolio names are not appropriate for a system with multiple users
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp_created = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Stock(models.Model):
 
     company_name = models.CharField(max_length=200, blank=True)
@@ -17,7 +28,8 @@ class Stock(models.Model):
     last_trade_price = models.DecimalField(max_digits=19, decimal_places=3, blank=True, null=True)
     timestamp_created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    #placing shares owned within Stock model is not appropriate for multiple users/portfolios
+    #placing shares owned and portfolio within Stock model is not appropriate for multiple users/portfolios
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT, blank=True, null=True)
     shares_owned = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
     market_value = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
 
@@ -29,7 +41,13 @@ class Stock(models.Model):
     def __str__(self):
         return self.symbol
 
+
     #add method to update market_value when shares_owned changes
+
+
+
+
+
 
 
 # implement below model for multiple users/portfolios
@@ -38,16 +56,6 @@ class Stock(models.Model):
 #     portfolio_id = models.ForeignKey(Portfolio, on_delete=models.PROTECT)
 #     stock_id = models.ForeignKey(Stock, on_delete=models.PROTECT)
 #     shares_owned = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
-#     timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-#     timestamp_created = models.DateTimeField(auto_now_add=True, auto_now=False)
-#
-#     def __str__(self):
-#         return self.id
-
-
-# implement below model for multiple users/portfolios
-# class Portfolio(models.Model):
-#
 #     timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 #     timestamp_created = models.DateTimeField(auto_now_add=True, auto_now=False)
 #
