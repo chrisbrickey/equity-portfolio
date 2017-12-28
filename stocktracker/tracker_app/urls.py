db56859974939380688c091d6834ae5215acc347
chrisbrickey <cbrickey1@gmail.com>
2017-12-27 19:00:14 -0800

    add API root and thread it to urls

--- stocktracker/tracker_app/urls.py
+++ stocktracker/tracker_app/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    url(r'^stocks/$', views.StockList.as_view()),
-    url(r'^stocks/(?P<pk>[0-9]+)/$', views.StockDetail.as_view()),
-]
+urlpatterns = format_suffix_patterns([
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+    url(r'^$', views.api_root),
+    url(r'^stocks/$', views.StockList.as_view(), name='stock-list'),
+    url(r'^stocks/(?P<pk>[0-9]+)/$', views.StockDetail.as_view(), name='stock-detail'),
+
+])
--- stocktracker/tracker_app/views.py
+++ stocktracker/tracker_app/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from rest_framework import generics, renderers
 
 from .models import Stock
 from .serializers import StockSerializer
 
-from rest_framework import generics
+# for API Root
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 class StockList(generics.ListCreateAPIView):
     queryset = Stock.objects.all()
@@ -14,3 +19,9 @@ class StockList(generics.ListCreateAPIView):
 class StockDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'stocks': reverse('stock-list', request=request, format=format)
+    })
