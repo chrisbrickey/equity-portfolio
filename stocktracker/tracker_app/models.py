acde421a9fe2a2338abdbd82df2c192c7743db1d
chrisbrickey <cbrickey1@gmail.com>
2017-12-28 13:52:55 -0800

    add buy_shares method and default value of last_trade_price to Stock model

--- stocktracker/tracker_app/models.py
+++ stocktracker/tracker_app/models.py
@@ -35,24 +35,28 @@ class Stock(models.Model):
 
     company_name = models.CharField(max_length=200, blank=True)
     symbol = models.CharField(max_length=20, unique=True, blank=False)
-    last_trade_price = models.DecimalField(max_digits=19, decimal_places=3, blank=True, null=True)
     timestamp_created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    #below are not appropriate for systems with multiple users or portfolios
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT, blank=True, null=True)
-    shares_owned = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
-    market_value = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
+    # last_trade_price = models.DecimalField(max_digits=19, decimal_places=3, blank=True, null=True) #defaults to None; preferred; using below for MVP
+    last_trade_price = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False) #defaults to 0.000 so buy_shares does not need to handle None type
 
     #below two should be combined in final version (e.g. update last_trade_price if last_updated == created OR last_updated > 5 seconds ago)
     last_trade_time = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    #below three are not appropriate for systems with multiple users or portfolios
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT, blank=True, null=True)
+    shares_owned = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
+    market_value = models.DecimalField(max_digits=19, decimal_places=3, default=Decimal('0.000'), blank=False)
+
     def __str__(self):
         return self.symbol
 
 
-    #add method to update market_value when shares_owned changes
-
+    #updates shares_owned and market_value; last_trade_price must be populated for this to work as expected; post-MVP, edit to handle last_trade_price as None type
+    def buy_shares(self, number_of_shares):
+        self.shares_owned = self.shares_owned + number_of_shares
+        self.market_value = self.last_trade_price * self.shares_owned
 
 
 
