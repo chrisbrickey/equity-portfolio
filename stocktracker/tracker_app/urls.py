906b16b8cef51b170bba0f0d57355e94d7db1afe
chrisbrickey <cbrickey1@gmail.com>
2017-12-27 17:54:54 -0800

    add suffix patterns to urls

--- stocktracker/tracker_app/urls.py
+++ stocktracker/tracker_app/urls.py
@@ -1,8 +1,11 @@
 #
 from django.conf.urls import url, include
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^stocks/$', views.StockList.as_view()),
     url(r'^stocks/(?P<pk>[0-9]+)/$', views.StockDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
