49f7c4850a91f10f76febde6561462431e1acca2
chrisbrickey <cbrickey1@gmail.com>
2017-12-29 02:00:03 -0800

    adapt serializers so that foreign key is stored as integer instead of object/url

--- stocktracker/tracker_app/serializers.py
+++ stocktracker/tracker_app/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Portfolio, Stock, LANGUAGE_CHOICES, STYLE_CHOICES
 
-
-class PortfolioSerializer(serializers.HyperlinkedModelSerializer):
+# ModelSerializer uses default versions of create() and update()
+# decided against HyperlinkedModelSerializer due to increased complexity when creating new objects with relationships
+class PortfolioSerializer(serializers.ModelSerializer):
     stock_set = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Portfolio
-        fields = ('url',        #we get this from HyperlinkedIdentityField
+        fields = ('url',
                   'id',
                   'name',
                   'stock_set',
@@ -16,12 +17,12 @@ class PortfolioSerializer(serializers.HyperlinkedModelSerializer):
         # restrict fields after determining what is required or add conditionals here to restrict data sent to frontend based on query string filters
 
 
-#previously used ModelSerializer as inheriting class which uses default versions of create() and update()
-class StockSerializer(serializers.HyperlinkedModelSerializer):
+
+class StockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stock
-        fields = ('url',        #we get this from HyperlinkedIdentityField
+        fields = ('url',
                   'id',
                   'symbol',
                   'company_name',
