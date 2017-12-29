5caae8d8593d0ed2d45fd48c4bd91fdb2c787099
chrisbrickey <cbrickey1@gmail.com>
2017-12-29 00:55:45 -0800

    add button to add stock to portfolio

--- stocktracker/stocktracker/urls.py
+++ stocktracker/stocktracker/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
 
     url(r'^stocks/$', views.stock_index, name='index'),
     url(r'^stocks/(?P<pk>[0-9]+)/$', views.stock_detail, name='detail'),
-    url(r'^retrieve/(?P<stock_sym>.+)/$', views.retrieve_stock_detail, name='manual-detail'),
+    url(r'^stocks/(?P<stock_sym>.+)/$', views.add_stock, name='add-stock'),
     
+    url(r'^retrieve/(?P<stock_sym>.+)/$', views.retrieve_stock_detail, name='manual-detail'),
+
     url(r'^search/$', views.render_search_form, name='search-form'),
     url(r'^alpha/$', views.search_stock, name='search-stock'),
     # url(r'^alpha/(?P<symbol>.+)/$', views.search_stock, name='search-stock'),
