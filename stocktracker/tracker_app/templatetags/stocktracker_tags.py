e0d5bfa8756ad63fe62c94da8b9c9d60c0e9abc7
chrisbrickey <cbrickey1@gmail.com>
2017-12-29 17:56:52 -0800

    finalize templatetag and template to perform market value calculation on frontend

--- stocktracker/tracker_app/templatetags/stocktracker_tags.py
+++ stocktracker/tracker_app/templatetags/stocktracker_tags.py
@@ -6,9 +6,3 @@ def multiply(a, b):
     interim = float(a) * float(b)
     rounded_string = str(round(interim, 3))
     return rounded_string
-
-@register.filter(name='market_value')
-def market_value(a, b):
-    interim = float(a) * float(b)
-    rounded_string = str(round(interim, 3))
-    return {'market_value': rounded_string}
