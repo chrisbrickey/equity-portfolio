4f3dba7cbb3b1535c6c42c6fe40e24a0093794a5
Chris Brickey <cbrickey1@gmail.com>
2025-11-23 12:50:18 +0100

    Remove unused code (mostly related to an earlier iteration of stocks)
    This also includes a minor refactor of the stock delete route to
    align the route name with django convention.

--- stocktracker/urls.py
+++ stocktracker/urls.py
@@ -34,14 +34,10 @@ urlpatterns = [
     re_path(r'^portfolios/(?P<pk>[0-9]+)/stocks/(?P<symbol>.+)/$', views.stock_detail_for_portfolio, name='portfolio-stock-detail'),
 
     # remove stock from portfolio
-    re_path(r'^stockDELETE/(?P<pk>[0-9]+)/$', views.delete_stock, name='delete-stock'),
+    re_path(r'^stock-delete/(?P<pk>[0-9]+)/$', views.delete_stock, name='delete-stock'),
 
     # refresh price data in a given portfolio
     re_path(r'^portfolio-refresh/(?P<pk>[0-9]+)/$', views.refresh_portfolio, name='refresh-portfolio'),
-
-    # dead code
-    re_path(r'^stocksOLD/$', views.stockOLD_index, name='stockOLD-index'),
-    re_path(r'^stocksOLD/(?P<pk>[0-9]+)/$', views.stockOLD_detail, name='stockOLD-detail'),
 
     # At root path, display the seeded portfolio
     re_path(r'^$', views.view_seeded_portfolio, name='view-seeded-portfolio'),
